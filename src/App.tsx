b3c956090294671bd0ead508c3950960b4a17185
devXamr <devxamr@gmail.com>
2025-03-07 16:24:18 +0530

    build minimal hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from "./Components/Navbar.tsx";
+import Hero from "./Components/Hero.tsx";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <div>
       <Navbar/>
+        <Hero/>
 
     </div>
   )
--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -1,16 +1,21 @@
+import { IoMdArrowDropright } from "react-icons/io";
+import heroImg from '../assets/Screenshot from 2025-03-07 16-07-09.png'
 export default function Hero(){
-    return <div>
-        <div>
-            <div>AIRBNB FOR YACH</div>
-            <div>Always By Your Side, Always On Your Side.</div>
-            <div>Most residences. Awarded the best agents. Suitable resources.</div>
-            <div>
-                <div>Read More</div>
-                <div>
-
+    return <div className='max-w-[80%] mx-auto flex justify-between mt-24'>
+        <div className='max-w-[50%] text-white'>
+            <div className='text-xl text-white'>AIRBNB FOR YACH</div>
+            <div className='text-6xl mr-11 mt-5'>Always By Your Side, Always On Your Side.</div>
+            <div className='text-2xl mt-4'>Most residences. Awarded the best agents. Suitable resources.</div>
+            <div className='flex mt-10'>
+                <div className=' border py-5 px-8 text-lg bg-transparent'>Read More</div>
+                <div className='bg-white'>
+                <IoMdArrowDropright className='text-black text-2xl mt-6 mx-3'/>
                 </div>
+
             </div>
         </div>
-        <div></div>
+        <div>
+            <img src={heroImg}/>
+        </div>
     </div>
 }
